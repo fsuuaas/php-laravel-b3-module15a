3cfa4060e2c6626af43c8e44ebd4ecae87bb078c
Sharif Uddin Ahamed <fsuuaas@gmail.com>
2024-06-14 11:57:40 +0600

    Assignment 3

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index($id): \Illuminate\Http\JsonResponse
+    {
+
+        $name = "Donal Trump";
+        $age = "18";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $cookieMinutes = 1;
+        $cookiePath = '/';
+        $cookieDomain = $_SERVER['SERVER_NAME'];
+        $cookieSecure = false;
+        $cookieHttpOnly = true;
+
+        $cookie = cookie(
+            $cookieName, $cookieValue,
+            $cookieMinutes,
+            $cookiePath,
+            $cookieDomain,
+            $cookieSecure,
+            $cookieHttpOnly
+        );
+
+        return response()->json($data, 200)->cookie($cookie);
+    }
+}
